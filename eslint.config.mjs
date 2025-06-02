af0438b94bda8ea63b7a3dcacc6e3e9da594c5fc
ikhwanNovertino <m.ikhwan.pilliang@gmail.com>
2025-06-02 19:08:12 +0800

    fix bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,15 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
-    ignorePatterns: [
-      '/src/generated/*'
-    ]
-  })
+  }
 ];
 
 export default eslintConfig;
